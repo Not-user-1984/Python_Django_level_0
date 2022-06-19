738e1894e03488e0b0d01413af9e4f0bc02cd33d
Not-user-1984 <deman.tmb.68@gmail.com>
2022-06-19 20:02:58 +0300

    unittest

--- firefoxdriver/funtional_test.py
+++ firefoxdriver/funtional_test.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
+
 driver = webdriver.Firefox (executable_path='/Users/diplug/Python _Projetc/Selenium_Python/firefoxdriver/geckodriver')
 
-driver.get('http://localhost:8000')
-assert 'Django' in driver.title
+
+class NewVizitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox (executable_path='/Users/diplug/Python _Projetc/Selenium_Python/firefoxdriver/geckodriver')
+
+    def tearDown(self):
+        self.driver.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.driver.get('http://localhost:8000')
+
+        self.assertIn('Django',self.driver.title)
+        self.fail('закочить тест')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
